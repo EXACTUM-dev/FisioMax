4f47c8cfa61b14cde4727e69f0b813a8677ffd23
MUCH0T3XT0 <a01276139@tec.mx>
2025-10-07 13:26:29 -0600

    Se agregan archivos del checklistModal

--- frontend/src/data/tableTemplates/rolePermissionsColumns.jsx
+++ frontend/src/data/tableTemplates/rolePermissionsColumns.jsx
@@ -6,6 +6,7 @@
 import React from "react";
 import trashIcon from "../../assets/icons/trash-2.png";
 import editIcon from "../../assets/icons/square-pen.png";
+import ConfirmModal from "../molecules/confirmationModal";
 
 /**
  * Build columns for: Rol, Permisos, Editar, Eliminar
