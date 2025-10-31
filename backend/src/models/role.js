5f1175df4369c1e48fca365bf5c1a4eb2a114996
MUCH0T3XT0 <a01276139@tec.mx>
2025-10-31 11:10:50 -0600

    Se hace correcion en consulta de RBAC para que sea mas facil de leer

--- backend/src/models/role.js
+++ backend/src/models/role.js
@@ -14,7 +14,23 @@ import { dbPool } from "../../config.js";
  */
 export const getUserRolesAndPermissions = async (idUsuario) => {
   const [rows] = await dbPool.query(
-    `SELECT r.nombre AS rol, p.nombre AS privilegio FROM Usuario u JOIN Membresia m ON u.IDUsuario = m.IDUsuario JOIN UsuarioRol ur ON u.IDUsuario = ur.IDUsuario JOIN Rol r ON ur.IDRol = r.IDRol JOIN RolPrivilegios rp ON r.IDRol = rp.IDRol JOIN Privilegio p ON rp.IDPrivilegio = p.IDPrivilegio WHERE u.clerkID = ? AND u.eliminado = 0 AND r.eliminado = 0 AND m.aceptado = 1 AND m.deletedAt IS NULL`,
+    `
+      SELECT
+        r.nombre AS rol,
+        p.nombre AS privilegio
+      FROM Usuario u
+      JOIN Membresia m ON u.IDUsuario = m.IDUsuario
+      JOIN UsuarioRol ur ON u.IDUsuario = ur.IDUsuario
+      JOIN Rol r ON ur.IDRol = r.IDRol
+      JOIN RolPrivilegios rp ON r.IDRol = rp.IDRol
+      JOIN Privilegio p ON rp.IDPrivilegio = p.IDPrivilegio
+      WHERE
+        u.clerkID = ?
+        AND u.eliminado = 0
+        AND r.eliminado = 0
+        AND m.aceptado = 1
+        AND m.deletedAt IS NULL
+    `,
     [idUsuario]
   );
 
