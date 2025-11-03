a92b1d8a61e3126acb9a158b2c69437585d5cc21
MUCH0T3XT0 <a01276139@tec.mx>
2025-11-03 00:13:49 -0600

    Correciones en nombre de tablas

--- backend/src/models/rbac.model.js
+++ backend/src/models/rbac.model.js
@@ -20,7 +20,7 @@ export const getUserRolesAndPermissions = async (clerkId) => {
         p.nombre AS privilegio
       FROM usuario u
       JOIN membresia m ON u.IDUsuario = m.IDUsuario
-      JOIN usuarioRol ur ON u.IDUsuario = ur.IDUsuario
+      JOIN usuariorol ur ON u.IDUsuario = ur.IDUsuario
       JOIN rol r ON ur.IDRol = r.IDRol
       JOIN rolprivilegios rp ON r.IDRol = rp.IDRol
       JOIN privilegio p ON rp.IDPrivilegio = p.IDPrivilegio
