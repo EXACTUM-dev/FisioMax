d8a1fc678124122c56541362ca2b1255521126cb
MUCH0T3XT0 <a01276139@tec.mx>
2025-11-19 18:28:35 -0600

    Salida a produccion de correos

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ import rolesRoutes from "./src/routes/roles.routes.js";
 import authRoutes from "./src/routes/auth.route.js";
 import homePageRoutes from "./src/routes/homePage.route.js";
 import loginLogsRoutes from "./src/routes/loginLogs.routes.js";
+import notificationsRoutes from "./src/routes/notifications.routes.js";
 
 // Initialize Express application
 const app = express();
@@ -96,6 +97,7 @@ app.use("/api/users", usuariosRoutes);
 app.use("/api/roles", rolesRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/logs", loginLogsRoutes);
+app.use("/api/notifications", notificationsRoutes);
 /**
  * Routes for video content access.
  */
---
+++ backend/src/routes/notifications.routes.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview API endpoints for ses notifications.
+ * @version 0.3.2
+ * @author EXACTUM-dev
+ */
+
+import express from "express";
+const router = express.Router();
+
+router.post("/ses/notifications", express.json(), async (req, res) => {
+  const messageType = req.headers["x-amz-sns-message-type"];
+  const message = req.body;
+
+  // Confirm the subscription
+  if (messageType === "SubscriptionConfirmation") {
+    const fetch = (await import("node-fetch")).default;
+    await fetch(message.SubscribeURL); // Confirm SNS subscription
+    return res.status(200).send("Subscription confirmed");
+  }
+
+  if (messageType === "Notification") {
+    const snsMessage = JSON.parse(message.Message);
+
+    if (snsMessage.notificationType === "Bounce") {
+      console.log("Bounce detected:", snsMessage);
+      // Marcar el correo como inválido en tu DB
+    }
+
+    if (snsMessage.notificationType === "Complaint") {
+      console.log("Complaint detected:", snsMessage);
+      // Desuscribir o pausar envíos a este correo
+    }
+
+    if (snsMessage.notificationType === "Delivery") {
+      console.log("Delivery:", snsMessage);
+    }
+  }
+
+  res.status(200).send("OK");
+});
+
+export default router;
