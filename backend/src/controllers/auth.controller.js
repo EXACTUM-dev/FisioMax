21ed1074521adb889471e1cb6eb15552ae577458
MUCH0T3XT0 <a01276139@tec.mx>
2026-02-27 15:58:23 -0600

    Se agregan logs para verificar acceso de usuarios

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -40,7 +40,9 @@ export const getProfile = async (req, res) => {
         }
 
         console.log(`[Login Success] Usuario con Clerk ID ${userId} (${userData.email || 'N/A'}) accedió correctamente y fue verificado en la base de datos.`);
-
+        console.log(userData.membershipPaymentStatus);
+        console.log(userPrivileges);
+        console.log(userData.membershipState);
         return res.json({
             success: true,
             user: {
